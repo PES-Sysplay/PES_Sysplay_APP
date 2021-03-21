9286fa0593b9505d022d3b055cdfbb72129b79b6
Eduard Puga Creus <eduard.puga@estudiantat.upc.edu>
2021-03-22 00:42:36 +0100

    problems fixed

--- app/src/main/java/com/example/workoutapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/workoutapp/ui/home/HomeFragment.java
@@ -12,23 +12,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.workoutapp.Activitat;
 import com.example.workoutapp.R;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private ActivityListAdapter adapter;
-    private List<Activitat> activityList;
+    private List<Activitat> activityList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = root.findViewById(R.id.activityTitle);
+        recyclerView = root.findViewById(R.id.recyclerview);
         updateList();
 
 
