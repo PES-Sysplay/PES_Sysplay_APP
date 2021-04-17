8b55fff2e2a002e54e62a15bd590a74260922850
JoelTej <79250969+JoelTej@users.noreply.github.com>
2021-04-17 13:14:55 +0200

    change password backend

--- app/src/main/java/com/example/workoutapp/UserController.java
+++ app/src/main/java/com/example/workoutapp/UserController.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 public class UserController {
 
     public static final String QUERY_FOR_LOGIN = "https://pes-workout.herokuapp.com/api/login/";
+    public static final String QUERY_FOR_CHANGE_PW = "https://pes-workout.herokuapp.com/api/change-password/";
     //aun no está implementada esta api creo
     public static final String QUERY_FOR_REGISTER = "https://pes-workout.herokuapp.com/api/register/";
     Context ctx;
@@ -71,6 +72,30 @@ public class UserController {
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(ctx, "Error al registrarse", Toast.LENGTH_SHORT).show();
                         vrl.onError("Error al registrarse");
+                    }
+                });
+        RequestSingleton.getInstance(ctx).addToRequestQueue(req);
+    }
+
+    public void changePass(UserController.VolleyResponseListener vrl) {
+        String url = QUERY_FOR_CHANGE_PW;
+
+        JsonObjectRequest req = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Gson gson = new Gson();
+                        User ret = gson.fromJson(response.toString(), User.class);
+
+                        vrl.onResponse(ret);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ctx, "Error al cambiar la contraseña", Toast.LENGTH_SHORT).show();
+                        vrl.onError("Error al cambiar la contraseña");
                     }
                 });
         RequestSingleton.getInstance(ctx).addToRequestQueue(req);
