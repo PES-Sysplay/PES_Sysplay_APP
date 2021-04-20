5a61227f9671f2d1d927377b47466866a2dff365
Eric <ersele46@gmail.com>
2021-04-21 01:03:31 +0200

    login register finished without db

--- app/src/main/java/com/example/workoutapp/LoginRegisterActivity.java
+++ app/src/main/java/com/example/workoutapp/LoginRegisterActivity.java
@@ -3,10 +3,13 @@ package com.example.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Toast;
 
+import com.example.workoutapp.ui.usermanage.GoogleManager;
 import com.example.workoutapp.ui.usermanage.LoginRegisterFragmentManager;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
@@ -48,6 +51,46 @@ public class LoginRegisterActivity extends AppCompatActivity {
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
                 //haha lol
+            }
+        });
+
+        bt_google.setOnClickListener(v -> {
+            int screen = tabLayout.getSelectedTabPosition();
+            UserController userController = new UserController(this);
+            GoogleManager googleManager = new GoogleManager(this);
+
+            GoogleSignInAccount u;
+            u = googleManager.signIn();
+
+            if (screen == 0){
+                userController.logIn(u.getDisplayName(), "", new UserController.VolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(LoginRegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String message) {
+                        Intent intent = new Intent(LoginRegisterActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+            } else if (screen == 1) {
+                userController.register(u.getEmail(), u.getDisplayName(), "", new UserController.VolleyResponseListener() {
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(LoginRegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(String message) {
+                        Intent intent = new Intent(LoginRegisterActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/workoutapp/User.java
+++ app/src/main/java/com/example/workoutapp/User.java
@@ -2,54 +2,26 @@ package com.example.workoutapp;
 
 public class User {
 
-    private String email;
+    private String token;
     private String username;
-    private String id;
     private String password;
-    private Boolean googleSigned; //If true password is empty
 
-    //default constructor
-    public User(){
-    }
-
-    //constructor for google sign-up
-    public User(String email, String username, String id, String password, Boolean googleSigned){
-        this.email = email;
+    public User(String token, String username, String password){
+        this.token = token;
         this.username = username;
-        this.id = id;
         this.password = password;
-        this.googleSigned = googleSigned;
     }
 
-    //constructor for sign-up without google
-    public User(String email, String username, String id, Boolean googleSigned){
-        this.email = email;
-        this.username = username;
-        this.id = id;
-        this.googleSigned = googleSigned;
+    public String getToken(){
+        return this.token;
     }
 
-    public Boolean getGoogleSigned() {
-        return googleSigned;
+    public String getUsername(){
+        return this.username;
     }
 
-    public String getEmail() {
-        return email;
+    public String getPassword(){
+        return this.password;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void changePassword(String newPass){
-        this.password = newPass;
-    }
 }
