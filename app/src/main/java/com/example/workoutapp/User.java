0731dc38e422c2ff66c8bbe31d178a1c7937be96
Eduard Puga Creus <eduard.puga@estudiantat.upc.edu>
2021-04-21 18:07:12 +0200

    Profile fragment finished and fragment home issue fix

--- app/src/main/java/com/example/workoutapp/User.java
+++ app/src/main/java/com/example/workoutapp/User.java
@@ -12,20 +12,11 @@ public class User {
         this.token = token;
         this.username = username;
         this.password = password;
-<<<<<<< HEAD
-        this.googleSigned = googleSigned;
+
     }
 
-    //constructor for sign-up without google
-    public User(String email, String username, String id, Boolean googleSigned){
-        this.email = email;
-        this.username = username;
-        this.id = id;
-        this.googleSigned = googleSigned;
-        //this.profilePic = defaultavatar.png;
-=======
->>>>>>> 5a61227f9671f2d1d927377b47466866a2dff365
-    }
+
+
 
     public String getToken(){
         return this.token;
