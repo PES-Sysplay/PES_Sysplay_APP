b28e39ae7c195f924e3621cf394e938346c55b32
victorripolles <victorripollesgarcia@gmail.com>
2021-05-26 14:07:09 +0200

    Fixing merge conflicts 2

--- app/src/main/java/com/example/workoutapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/workoutapp/ui/profile/ProfileFragment.java
@@ -1,29 +1,26 @@
 package com.example.workoutapp.ui.profile;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+
 import com.example.workoutapp.LoginRegisterActivity;
 import com.example.workoutapp.R;
 import com.example.workoutapp.UserController;
 import com.example.workoutapp.UserSingleton;
-import com.example.workoutapp.ui.myactivities.MyActivitiesActivity;
 import com.example.workoutapp.ui.usermanage.SharedPreferencesController;
 
 import java.util.ArrayList;
@@ -84,9 +81,6 @@ public class ProfileFragment extends Fragment {
         view.findViewById(R.id.my_activities).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Context context = view.getContext();
-//                Intent intent = new Intent(context, MyActivitiesActivity.class);
-//                context.startActivity(intent);
                 NavHostFragment.findNavController(ProfileFragment.this)
                         .navigate(R.id.action_navigation_profile_to_my_activities_fragment);
 
